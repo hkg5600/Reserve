101b2d9d8a37878aba0814833e69f7ea97fabb04
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2019-11-21 19:17:06 +0900

    Add: LoginActivity

---
+++ reserve/app/src/main/java/com/example/reserve/ui/login/LoginActivity.kt
@@ -0,0 +1,56 @@
+package com.example.reserve.ui.login
+
+import android.content.Intent
+import android.util.Log
+import androidx.lifecycle.Observer
+import com.example.reserve.R
+import com.example.reserve.base.BaseActivity
+import com.example.reserve.databinding.ActivityLoginBinding
+import com.example.reserve.network.response.TokenResponse
+import com.example.reserve.room.repository.TokenRepository
+import com.example.reserve.ui.main.MainActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LoginActivity : BaseActivity<ActivityLoginBinding, LoginActivityViewModel>() {
+
+    override val layoutResourceId = R.layout.activity_login
+
+    override val viewModel: LoginActivityViewModel by viewModel()
+
+    override fun initView() {
+
+    }
+
+    override fun initObserver() {
+        viewModel.data.observe(this, Observer {
+            when (it) {
+                is TokenResponse -> {
+                    viewModel.insertToken(it.token)
+                }
+            }
+        })
+
+        viewModel.roomSuccess.observe(this, Observer {
+            when(it) {
+                "token" -> startActivity(Intent(this, MainActivity::class.java))
+            }
+        })
+
+        viewModel.error.observe(this, Observer {
+            Log.d("Observe Error: ", it)
+            when(it) {
+                "room failed" -> makeToast("오류 발생 잠시후 시도해주세요", false)
+            }
+        })
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initViewModel() {
+        viewDataBinding.vm = viewModel
+    }
+
+
+}
