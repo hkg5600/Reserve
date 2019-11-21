fecfddc1ed9f7af993dfb8dcf1e01a2155eb2e46
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2019-11-21 16:01:00 +0900

    Add: Splash Activity

---
+++ reserve/app/src/main/java/com/example/reserve/ui/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.reserve.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.reserve.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val h = Handler()
+        h.postDelayed(SplashHandler(), 3000)
+    }
+
+    inner class SplashHandler : Runnable {
+        override fun run() {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
+
+
+
+    override fun onBackPressed() {
+
+    }
+}
